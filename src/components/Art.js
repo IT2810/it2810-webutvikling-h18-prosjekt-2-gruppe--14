6ea2f583a76e4ab6d0c500e1ea0a5f407eaa056e
Unknown <kiddid96@gmail.com>
2018-09-18 16:43:55 +0200

    Initialize Art class #2

---
+++ src/components/Art.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Art extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            motive: 0,
+            sound: 0,
+            text: 0
+        }
+    }
+
+    requestMedia() {}
+    render() {}
+}
